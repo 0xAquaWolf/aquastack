e2cc342c81b09b4f417cc912572ede86e670e606
0xAquaWolf <aquawolf369@gmail.com>
2025-09-29 03:44:07 -0500

    updates

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,25 +1,34 @@
 import '../global.css'
 
+import { StrictMode } from 'react'
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import { Stack } from 'expo-router'
+import { authClient } from '../lib/auth-client'
+import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react'
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 }
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL! as string, {
   unsavedChangesWarning: false,
+  // Optionally pause queries until the user is authenticated
+  expectAuth: true,
 })
 
 export default function RootLayout() {
   return (
     // @ts-ignore
-    <ConvexProvider client={convex}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ConvexProvider>
+    <StrictMode>
+      <ConvexProvider client={convex}>
+        <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          </Stack>
+        </ConvexBetterAuthProvider>
+      </ConvexProvider>
+    </StrictMode>
   )
 }
