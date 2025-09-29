a866752e8f1e000144e4db4ad7380212a3b09489
0xAquaWolf <aquawolf369@gmail.com>
2025-09-28 21:11:37 -0500

    updates

--- apps/convex/convex/tasks.ts
+++ apps/convex/convex/tasks.ts
@@ -3,6 +3,7 @@ import { query } from './_generated/server'
 export const get = query({
   args: {},
   handler: async (ctx) => {
+    console.log(await ctx.db.query('tasks').collect())
     return await ctx.db.query('tasks').collect()
   },
 })
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,8 @@ import { useQuery } from "convex/react";
 import { api } from "@svq/convex";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get)
+  const tasks = useQuery(api.tasks.get);
+  console.log(tasks);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
